dfa8f0ad3bfb6417a841cdf0654f9b7e87a3d2ca
BishnoiAmit <38841690+BishnoiAmit@users.noreply.github.com>
2018-04-29 12:28:45 -0700

    Code
    Uploaded Project Code.

---
+++ autonomy.py
@@ -0,0 +1,108 @@
+import RPi.GPIO as gpio
+import time
+import sys
+import Tkinter as tk
+from sensor import distance
+import random
+
+
+
+def init():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(7,gpio.OUT)
+    gpio.setup(11,gpio.OUT)
+    gpio.setup(13,gpio.OUT)
+    gpio.setup(15,gpio.OUT)
+
+def turn_left(tf):
+
+
+    gpio.output(7,False)
+    gpio.output(11,True)
+    gpio.output(13,True)
+    gpio.output(15,False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def turn_right(tf):
+    
+    gpio.output(7,False)
+    gpio.output(11,True)
+    gpio.output(13,False)
+    gpio.output(15,True)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def forward(tf):
+    
+    gpio.output(7,False)
+    gpio.output(11,True)
+    gpio.output(13,False)
+    gpio.output(15,False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def reverse(tf):
+   
+    gpio.output(7,True)
+    gpio.output(11,False)
+    gpio.output(13,False)
+    gpio.output(15,False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+
+def check_front():
+    init()
+    dist = distance()
+
+    if dist<40:
+        print('Too close,need to move back!!',dist)
+        init()
+        reverse(2)
+        init()
+        turn_right(2)
+        dist= distance()
+        if dist < 40:
+            print('Too Close,Reassessing Path' , dist)
+            init()
+            turn_left(2)
+            init()
+            reverse(2)
+            dist = distance()
+            if dist < 40:
+                print('Too Close, giving up', dist)
+                sys.exit()
+
+
+
+def autonomy():
+    tf=0.030
+    x=random.randrange(0,4)
+
+    if x == 0:
+        for y in range(30):
+            check_front()
+            init()
+            forward(tf)
+    elif x == 1:
+        for y in range(30):
+            check_front()
+            init()
+            forward(tf)
+    elif x == 2:
+        for y in range(30):
+            check_front()
+            init()
+            turn_right(tf)
+    elif x == 3:
+        for y in range(30):
+            check_front()
+            init()
+            turn_left(tf)
+
+for z in range(10):
+    autonomy()
+    
+       
+                       
